95cf5b46e3fcf55e79d4d17384a71f3fa226247e
bugbat <85291787+bugbat@users.noreply.github.com>
2024-07-31 09:48:49 -0400

    update style

--- src/about.js
+++ src/about.js
@@ -1,7 +1,7 @@
 export default function buildAbout() {
   const frag = document.createDocumentFragment();
   let para = document.createElement('p');
-  para.textContent = 'JabberWok was founded in 2012 by two mixed up dreamers who wanted to make a restaurant that would fail so gloriously it would end up as a B plot on an IFC original.';
+  para.textContent = 'JabberWok was founded in 2012 Lorem ipsum odor amet, consectetuer adipiscing elit. Sodales pharetra mattis, tincidunt scelerisque adipiscing accumsan. Taciti parturient id egestas placerat pellentesque. Malesuada etiam iaculis lacus purus integer urna nisl id orci. Porta eros sapien ultricies erat suspendisse purus fames laoreet. Ornare semper imperdiet non amet dolor.';
 
   frag.append(para);
 
